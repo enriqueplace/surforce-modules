7bff1ae1298eb99581c04138b299fa3e47ef0f25
enriqueplace <enriqueplace@f9a29365-1644-0410-aa76-11d68dfb1bdc>
2008-03-11 16:28:20 +0000

    Primera versión de autenticación (autor Pablo Morales)

---
+++ autentication/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+// FIXME!
+require_once './application/autentication/models/Autentication.php';
+
+class Autentication_IndexController extends Zsurforce_Generic_Controller{
+	public	function indexAction()
+	{
+		$autentication = new Autentication();
+		$where = array();
+		$order = "id";
+		$this->view->Autentication = $autentication->fetchAll($where, $order);
+		$this->view->base_path = Zend_Registry::get('base_path');
+		$this->render();
+	}
+}
+?>
