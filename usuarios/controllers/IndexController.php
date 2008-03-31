86a0036464eba71780f8891d452a7e3afe36a02b
jantunez <jantunez@f9a29365-1644-0410-aa76-11d68dfb1bdc>
2008-03-31 20:55:50 +0000

    Cambios

--- usuarios/controllers/IndexController.php
+++ usuarios/controllers/IndexController.php
@@ -1,13 +1,12 @@
 <?php
 require_once './application/usuarios/models/Usuarios.php';
 require_once ("Zend/Translate.php");
-class Usuarios_IndexController extends Zsurforce_Generic_Controller{
-    function init()
-    {
-        parent::init();
-        Zend_Loader::loadClass('Usuarios');
-        $lenguage = Zend_Registry::get ( 'config_sys' )->regional->lenguage;
-    try {
+class Usuarios_IndexController extends Zsurforce_Generic_Controller {
+	function init() {
+		parent::init ();
+		Zend_Loader::loadClass ( 'Usuarios' );
+		$lenguage = Zend_Registry::get ( 'config_sys' )->regional->lenguage;
+		try {
 			$translate = new Zend_Translate ( 'csv', './application/usuarios/views/lenguage/' . $lenguage . '.csv', $lenguage );
 		} catch ( Exception $e ) {
 			// File not found, no adapter class...
@@ -15,29 +14,28 @@ class Usuarios_IndexController extends Zsurforce_Generic_Controller{
 			$lenguage = "es";
 			$translate = new Zend_Translate ( 'csv', $this->view->base_path . '/application/usuarios/views/lenguage/' . $lenguage . '.csv', $lenguage );
 		}
-		//Construyo de forma dinámica los textos para mostrar en la vista 
+		//Construyo de forma dinï¿½mica los textos para mostrar en la vista 
 		//de acuerdo a lo ingresado en el archivo de idioma
 		foreach ( $translate->getAdapter ()->getMessageIds () as $identificador ) {
 			$this->view->$identificador = $translate->_ ( $identificador, $lenguage );
 		}
-    }
-    function preDispatch()
-    {
-        $auth = Zend_Auth::getInstance();
-       
-       // if ($auth->hasIdentity()) {
-        //    $this->view->usuarioLogueado = true;
-        //}else{
-          //  die( "Acceso Restringido");
-        //}
-    }
-
-
-    function indexAction(){
-        $usuarios = new Usuarios();
-        $this->view->usuarios = $usuarios->fetchAll();
-        $this->render();
-    }
-    
+	}
+	function preDispatch() {
+		$auth = Zend_Auth::getInstance ();
+		
+	// if ($auth->hasIdentity()) {
+	//    $this->view->usuarioLogueado = true;
+	//}else{
+	//  die( "Acceso Restringido");
+	//}
+	}
+	
+	function indexAction() {
+		$usuarios = new Usuarios ( );
+		$this->view->usuarios = $usuarios->fetchAll ();
+		$this->render ();
+	}
+	
+	
 }
 ?>
